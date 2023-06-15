735957f0b51a135bfcd20ad99f7b2845903d7373
Noelle <noelle.maingi@student.moringaschool.com>
2023-06-16 00:15:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+let name = ["Guadalupe", "Ollie", "Aki"];
+
+let occasion = "surprise";
+
+function writeCards(names, occasion) {
+  const thankyouMessage = [];
+  for (let i = 0; i < names.length; i++) {
+    thankyouMessage.push(
+      `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`
+    );
+  }
+  return thankyouMessage;
+}
+
+let log;
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
